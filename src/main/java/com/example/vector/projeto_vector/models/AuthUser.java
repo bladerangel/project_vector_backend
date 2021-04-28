5b3f2127ed15a29a02811ab2c93ae3b1fc521ea1
bladerangel <bladerangel10@gmail.com>
2021-04-28 16:59:36 -0300

    Create auth user model

---
+++ src/main/java/com/example/vector/projeto_vector/models/AuthUser.java
@@ -0,0 +1,82 @@
+package com.example.vector.projeto_vector.models;
+
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.example.vector.projeto_vector.listeners.AuthUserListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "email" }) })
+@EntityListeners(AuthUserListener.class)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthUser implements UserDetails {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Email
+  private String email;
+
+  @JsonProperty(access = Access.WRITE_ONLY)
+  @NotBlank
+  private String password;
+
+  @Override
+  @JsonIgnore
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  @JsonIgnore
+  public String getUsername() {
+    return null;
+  }
+
+  @Override
+  @JsonIgnore
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  @JsonIgnore
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  @JsonIgnore
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  @JsonIgnore
+  public boolean isEnabled() {
+    return true;
+  }
+
+}
