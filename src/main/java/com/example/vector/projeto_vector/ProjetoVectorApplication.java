64d3ef3aa8ab3ca31070066f8436eec139b5ba24
bladerangel <bladerangel10@gmail.com>
2021-04-28 16:57:03 -0300

    Create project spring boot

---
+++ src/main/java/com/example/vector/projeto_vector/ProjetoVectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.vector.projeto_vector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vector/projeto_vector/ProjetoVectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vector.projeto_vector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
